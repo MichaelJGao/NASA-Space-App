e1ef076203c52d65d2c116e815da3fea592224a1
MichaelJGao <107709020+MichaelJGao@users.noreply.github.com>
2023-10-07 23:17:52 -0600

    init working

--- views.py
+++ views.py
@@ -4,4 +4,12 @@ views = Blueprint('views', __name__)
 
 @views.route("/")
 def home():
+    return render_template('home.html')
+
+@views.route("/ocean")
+def ocean():
     return render_template('ocean.html')
+
+@views.route("/earth2")
+def earth2():
+    return render_template('earth2.html')
